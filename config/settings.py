3bcf88235c1586fccbedf68eca805c40115d9093
HCY <81405114@gcloud.csu.edu.tw>
2025-11-16 16:34:34 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -67,3 +67,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
